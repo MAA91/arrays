60b38d6a42522bd32645ff826333b04952c6eb1e
User <User@DESKTOP-GE3OG10>
2024-02-05 21:38:22 +0300

    Introduction to arrays

--- arrays/mai.cpp
+++ arrays/mai.cpp
@@ -5,5 +5,11 @@ using namespace std;
 void main()
 {
 	setlocale(0, "");
-
+	const int SIZE = 5;
+	int A[SIZE] = {};
+	for (int i = 0; i < SIZE; i++)
+		cin >> A[i];
+	for (int i = 0; i < SIZE; i++)
+		cout << A[i] << "\t";
+	cout << endl;
 }
