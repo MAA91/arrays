7557c29ada7d68c79f83d54a40dcd49af0aa091d
User <User@DESKTOP-GE3OG10>
2024-02-11 15:20:49 +0300

    В Solution 'Arrays' добавить проект 'Shift', и в этом проекте есть массив из 10 элементов,    проинициализированный числами Фибоначчи. Нужно выполнить циклический сдвиг этого массива    на заданное число элементов влево.

---
+++ Shift/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(0, "");
+	const int SIZE = 10;
+	int A[SIZE] = {}, sdvig;
+	cout << "¬ведите число на которое нужно выполнить сдвиг: "; cin >> sdvig;
+	A[0] = A[1] = 1;
+	for (int i = 2; i < SIZE; i++)
+		A[i] = A[i - 1] + A[i - 2];
+	for (int i = 0; i < sdvig % SIZE; i++)
+	{
+		int buf = A[0];
+		for (int j = 1; j < SIZE; j++)
+			A[j - 1] = A[j];
+		A[SIZE - 1] = buf;
+	}
+	for (int i = 0; i < SIZE; i++)
+		cout << A[i] << "\t";
+}
