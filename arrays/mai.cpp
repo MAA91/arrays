5e5182a4f81406209550508aedf9c8bd9f12bb41
User <User@DESKTOP-GE3OG10>
2024-02-11 14:39:30 +0300

    В проекте 'Arrays' решить следующие задачи:  -ввести значения элементов массива с клавиатуры;  -вывести элементы массива на экран в прямом порядке (от нулевого элемента до последнего);  -вывести элементы массива на экран в обратном порядке (от последнего элемента до нулевого);  -вычислить сумму элементов массива;  -вычислить среднее-арифметическое элементов массива;  -найти минимальное и максимальное значения в массиве, и вывести их на экран;

--- arrays/mai.cpp
+++ arrays/mai.cpp
@@ -6,10 +6,27 @@ void main()
 {
 	setlocale(0, "");
 	const int SIZE = 5;
-	int A[SIZE] = {};
+	int A[SIZE] = {}, max = 0, min = 0;
+	double sum = 0;
 	for (int i = 0; i < SIZE; i++)
+	{
 		cin >> A[i];
-	for (int i = 0; i < SIZE; i++)
 		cout << A[i] << "\t";
+		sum += A[i];
+	}
 	cout << endl;
+	max = min = A[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << A[SIZE - 1 - i] << "\t";
+		if (A[i] > max)
+			max = A[i];
+		if (A[i] < min)
+			min = A[i];
+	}
+	cout << endl;
+	cout << "Сумма равна: " << sum << endl;
+	cout << "Среднее арифметическое равно: " << sum/SIZE << endl;
+	cout << "Максимальное значение равно: " << max << endl;
+	cout << "Минимальное значение равно: " << min << endl;
 }
