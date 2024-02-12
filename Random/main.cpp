593fba90d1727f67c9f38cd5a656406b1fce3907
User <User@DESKTOP-GE3OG10>
2024-02-12 23:14:28 +0300

    В Solution 'Arrays' добавить проект 'Random', и в этом проекте заполнить массив случайными числами,    в диапазоне заданном пользователем, вывести его на экран, затем отсортировать, и снова вывести на экран,    уже отсортированный массив.

---
+++ Random/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(0, "");
+	srand(time(NULL));
+	const int SIZE = 10;
+	int A[SIZE] = {}, diapazone_1, diapazone_2;
+	cout << "¬ведите ганицы диапазона: "; cin >> diapazone_1 >> diapazone_2;
+	int size = abs(diapazone_1 - diapazone_2) + 1;
+	for (int i = 0; i < SIZE; i++)
+	{
+		A[i] = diapazone_1 + rand() % size;
+		cout << A[i] << "\t";
+	}
+	cout << endl;
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 1; j < SIZE - i; j++)
+			if (A[j - 1] > A[j])
+			{
+				int buf = A[j];
+				A[j] = A[j - 1];
+				A[j - 1] = buf;
+			}
+	for (int i = 0; i < SIZE; i++)
+		cout << A[i] << "\t";
+	cout << endl;
+}
